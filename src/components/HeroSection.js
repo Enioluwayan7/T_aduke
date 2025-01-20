ea124275150688cc7eeb1fb4d3ed62f4139228fc
Enioluwayan7 <122826740+Enioluwayan7@users.noreply.github.com>
2025-01-20 18:41:34 +0000

    Update all to the git hub

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,6 @@ function HeroSection() {
     <section className="hero" id="home">
       <div className="hero-content">
         <h1>Step into Style</h1>
-        <i class="bi bi-house-heart"></i>
         <p>Discover the latest fashion trends with us.</p>
         <a href="#products" className="shop-now">Shop Now</a>
       </div>
