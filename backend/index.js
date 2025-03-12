7768315028e43ab622b3c0ab9a3016dc5445ff87
Anagho <anaghosamroyce@gmail.com>
2025-03-12 23:49:28 +0100

    pulled successfully

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import express from "express";
-
-const PORT = 5000;
-const app = express();
-
-app.listen(PORT, async () => {
-  console.log(`Server running on PORT: ${PORT}`);
-=======
 import "dotenv/config";
-
-import express from "express";
 import cors from "cors";
 
 const PORT = process.env.PORT || 4000;
@@ -29,5 +19,4 @@ app.get("/health", (req, res) => {
 
 app.listen(PORT, async () => {
   console.log(`Server is running on PORT: ${PORT}`);
->>>>>>> 5cfbe03602fcd6a010a94d8cb5e962e75950f629
 });
