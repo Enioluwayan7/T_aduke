26fefe729d65eb7ac02eaf4dd37fedb6b9ac541b
festusfolarin <maicharles001@gmail.com>
2025-03-14 00:34:45 +0100

    adding user and order routes and controllers

---
+++ backend/controllers/userControllers/registerUser.js
@@ -0,0 +1,63 @@
+// import { UserModel } from "../../models/userModel/userModel.js";
+import bcrypt from "bcrypt";
+
+async function registerUser(req, res) {
+  const { email, name, password, confirmPassword } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "All fields are required!",
+      status: "failed",
+    });
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({
+      message: "Invalid email format!",
+      status: "failed",
+    });
+  }
+
+  if (password !== confirmPassword) {
+    return res
+      .status(400)
+      .json({ message: "Passwords don't match!", status: "failed" });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: "Password must be at least 6 characters long!",
+      status: "failed",
+    });
+  }
+
+  if (!name) {
+    return res.status(400).json({
+      message: "Name is required!",
+      status: "failed",
+    });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  //register user here
+
+  //   try {
+  //     const user = new UserModel({
+  //       name: name,
+  //       email: email,
+  //       password: hashPassword,
+  //     });
+  //     await user.save();
+
+  //     res
+  //       .status(201)
+  //       .json({ message: "Account created successfully", status: "successful" });
+  //   } catch (error) {
+  //     res.status(400).json({ message: error.message, status: "failed" });
+  //   }
+}
+
+export { registerUser };
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -17,6 +18,8 @@ app.use(express.json()); //to access data from frontend
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is currently running" });
 });
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Server is running on PORT: ${PORT}`);
